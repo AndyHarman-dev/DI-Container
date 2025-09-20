2b4daf877e3488248166e7a75f214c00592c051a
Wiam <85005549+BeginOneSomeSh1t@users.noreply.github.com>
2025-09-20 07:55:12 -0300

    Update DIContainerModule.Build.cs

--- DIContainerModule.Build.cs
+++ DIContainerModule.Build.cs
@@ -18,6 +18,7 @@ public class DIContainerModule : ModuleRules
             {
                 "CoreUObject",
                 "Engine",
+                "UELoggingModule"
             }
         );
     }
